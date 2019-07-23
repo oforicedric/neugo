145b921819b7ac459435f93f30ab863600e00608
oforicedric <42480269+oforicedric@users.noreply.github.com>
2019-07-23 22:49:14 +0100

    Update manage.py

--- manage.py
+++ manage.py
@@ -2,7 +2,7 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-
+#default function, required for settings
 
 def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'workout_project.settings')
