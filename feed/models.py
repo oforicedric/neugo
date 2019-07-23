aa2a547809d40781457f8ccff03eb90975c15b25
oforicedric <42480269+oforicedric@users.noreply.github.com>
2019-07-23 23:01:28 +0100

    Update models.py

--- feed/models.py
+++ feed/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
 
+#stores attributes to create a post
 class Post(models.Model):
 	title = models.CharField(max_length=200)
 	content = models.TextField()
