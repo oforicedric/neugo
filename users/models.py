abf471cda0a6e93ddae8d09d8b3fba0c758eb7e7
oforicedric <42480269+oforicedric@users.noreply.github.com>
2019-07-23 22:38:10 +0100

    Update models.py

--- users/models.py
+++ users/models.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import User
 from django.db import models
 from PIL import Image
 
-
+# profile model, attibutes can be added using models class
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default='default.jpg', upload_to='profile_pics')
