1dd2e62dab23e7b2a28f6c0c770284208b6a58f2
Vinteuil <augustine.mavor-parker@kagenova.com>
2019-09-18 22:49:54 +0100

    made changes so you can run locally

--- django_project/settings.py
+++ django_project/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-import django_heroku 
+#import django_heroku 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -27,8 +27,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 #DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 DEBUG = 'False'
 
-ALLOWED_HOSTS = ['neugo-learning.herokuapp.com']
-#ALLOWED_HOSTS = ['127.0.0.1']
+#ALLOWED_HOSTS = ['neugo-learning.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1']
 
 # Application definition
 
@@ -144,4 +144,4 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
 
-django_heroku.settings(locals())
+#django_heroku.settings(locals())
