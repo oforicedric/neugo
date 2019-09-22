e1bee2811ac80731116f89ea958252c3602583a8
oforicedric <oforicedric@icloud.com>
2019-09-22 15:54:33 +0100

    updated home.html

--- django_project/settings.py
+++ django_project/settings.py
@@ -21,14 +21,14 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = 'Gunners'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 DEBUG = 'False'
 
 #ALLOWED_HOSTS = ['neugo-learning.herokuapp.com']
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1:8000', 'localhost', 'localhost:8000']
 
 # Application definition
 
