ea27056e51e4e05aee5e83536648d49e4b45584f
vinteuil <zcapmav@ucl.ac.uk>
2019-12-24 18:12:07 +0000

    added field for new user attributes

--- users/models.py
+++ users/models.py
@@ -1,4 +1,4 @@
-import random 
+import random
 from django.db import models
 from django.contrib.auth.models import User
 from PIL import Image
@@ -15,15 +15,16 @@ class Profile(models.Model):
         "science_student.jpeg",
         "student_sitting_on_grass.jpg",
     ]
-    
-    for i in range(len(profile_pictures)): 
-        profile_pictures[i] = 'potential_pics/' + profile_pictures[i]
-    
+
+    for i in range(len(profile_pictures)):
+        profile_pictures[i] = "potential_pics/" + profile_pictures[i]
+
     image = models.ImageField(
-        default=random.choice(profile_pictures),
-        upload_to="profile_pics",
+        default=random.choice(profile_pictures), upload_to="profile_pics"
     )
     wallet_points = models.BigIntegerField(default=0)
+    university = models.CharField(default="", max_length=35)
+    subject = models.CharField(default="", max_length=35)
 
     def __str__(self):
         return f"{self.user.username} Profile"
