091175dffb4ed19fb357565d7b2be6125c1e8b34
vinteuil <zcapmav@ucl.ac.uk>
2019-12-26 03:54:31 +0000

    partly register details from onboarding

--- users/views.py
+++ users/views.py
@@ -11,6 +11,7 @@ def register(request):
             form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'Your account has been created! You are now able to log in')
+            request.user.university = request.session['university']
             return redirect('login')
     else:
         form = UserRegisterForm()
